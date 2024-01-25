86ed09a454b82e896ed427aa9e6b6199b2ac08f1
granth2023 <grantharris38@gmail.com>
2024-01-25 10:11:19 -0500

    year variable filled

--- backend/moviecentralpythonapp/models.py
+++ backend/moviecentralpythonapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=200)
-    year
+    year = models.CharField(max_length=4)
     rated
     released
     runtime
