6495d1e588b77a0034961ad34f4b0881968973e9
granth2023 <grantharris38@gmail.com>
2024-01-25 10:11:55 -0500

    release vairable

--- backend/moviecentralpythonapp/models.py
+++ backend/moviecentralpythonapp/models.py
@@ -5,8 +5,7 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=200)
     year = models.CharField(max_length=4)
-    rated
-    released
+    released = models.DateField()
     runtime
     genre
     director
