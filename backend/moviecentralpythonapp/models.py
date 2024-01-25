c08ad9ec51d7d641e599f3e65c0485fa71df9de1
granth2023 <grantharris38@gmail.com>
2024-01-25 10:12:14 -0500

    runtime variable

--- backend/moviecentralpythonapp/models.py
+++ backend/moviecentralpythonapp/models.py
@@ -6,7 +6,7 @@ class Movie(models.Model):
     title = models.CharField(max_length=200)
     year = models.CharField(max_length=4)
     released = models.DateField()
-    runtime
+    runtime = models.CharField(max_length=30)
     genre
     director
     actors
