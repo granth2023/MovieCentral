b57d03fa7fb5008c03392c237063085b3205430c
granth2023 <grantharris38@gmail.com>
2024-01-26 11:43:15 -0500

    update import generic foreign key

--- backend/moviecentralpythonapp/models.py
+++ backend/moviecentralpythonapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.contenttypes.models import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import AbstractUser
